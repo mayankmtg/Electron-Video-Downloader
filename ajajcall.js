39814a591a7be8339ec54ff8de77dcf699479a34
mayank mohindra <mayank mohindra>
2017-03-11 23:43:05 +0530

    Download in chunks

--- ajajcall.js
+++ ajajcall.js
@@ -1,6 +1,10 @@
 var btn = document.getElementById("loader");
 var maincontent=document.getElementById("maincontent");
 var counter=0;
+var shell = require('shelljs');
+//shell.config.execPath('./curl.exe');
+//shell.common.config.execPath('./curl.exe');
+var curl = require('curlrequest');
 var ourData;
 btn.addEventListener("click", function(){
 	if(counter==0){
@@ -28,22 +32,26 @@ function setHTML(data){
 }
 
 function Download(num){
-	//console.log(ourData[num].name);
+	console.log(ourData[num].name);
+	shell.echo(ourData[num].name);
 	var link=ourData[num].link;
 	var name=ourData[num].name;
 	var size=ourData[num].size;
 	var chunk=1024;
 	var n=size/chunk;
 	var i;
+	//var shell1=require('shelljs');
+
 	for(i=0;i<n-1;i++){
-		if (shell.exec('curl -r 'chunk*i+'-'+(chunk)*(i+1)-1+ ' -o '+name+'_'+i+'.png '+link).code !== 0) {
+
+		if (shell.exec('curl -r '+(chunk*i)+'-'+((chunk)*(i+1)-1)+' -o '+name+'_'+i+'.png '+link).code !== 0) {
 			shell.echo('Error');
 			shell.exit(1);
 		}else{
 			console.log("Chunk "+ i);	
 		}
 	}
-	if (shell.exec('curl -r 'chunk*n-1+'- -o '+name+'_'+n-1+'.png '+link).code !== 0) {
+	if (shell.exec('curl -r '+(chunk*n-1)+'- -o '+name+'_'+n-1+'.png '+link).code !== 0) {
 		shell.echo('Error');
 		shell.exit(1);
 	}
@@ -51,4 +59,20 @@ function Download(num){
 		console.log("Chunk "+ n-1);	
 		console.log("Finished");	
 	}
+	console.log("Merging");
+	if (shell.exec('head -c -1 -q '+name+'_*.jpg > '+name+'.jpg').code !== 0) {
+		shell.echo('Error');
+		shell.exit(1);
+	}
+	else{
+		console.log("Merge Finished");	
+	}	
+	console.log("Deleting");
+	if (shell.exec('rm '+name+'_* ').code !== 0) {
+		shell.echo('Error');
+		shell.exit(1);
+	}
+	else{
+		console.log("Delete Finished");	
+	}	
 }
